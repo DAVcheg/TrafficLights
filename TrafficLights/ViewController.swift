2fdef367facf8e7c48c4ebc2dbe6948756c45bc8
Dergunov Aleksey <davcheg@me.com>
2022-01-10 20:26:07 +1000

    Visual layout with constrains

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var showTextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        showTextButton.layer.cornerRadius = 20
+        redLightView.layer.cornerRadius = 100
+        yellowLightView.layer.cornerRadius = 100
+        greenLightView.layer.cornerRadius = 100
     }
 
-
+    @IBAction func switchLightButton(_ sender: Any) {
+    }
+    
 }
 
